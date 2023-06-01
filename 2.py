73730c710d8edb88736c31332b795f6501880794
Mani924 <chamarthi509@gmail.com>
2023-06-01 18:03:57 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,11 +1,16 @@
-a=raw_input("Enter Actual String")
-b=raw_input("Enter Sub String")
+import boto3
 
-def string(a,b):
-    a.lower()
-    b.lower()
-    if b in a:
-        print "Substring is in actual string"
-    else:
-        print "Substring not found"
-string(a,b)
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    bucket_name = 'your-bucket-name'
+    key_prefix = 'your-folder-prefix'  # Optional: If you want to copy files from a specific folder within the bucket
+    
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=key_prefix)
+    
+    for file in response['Contents']:
+        file_key = file['Key']
+        file_name = file_key.split('/')[-1]  # Extract the file name from the key
+        
+        # Copy file from S3 bucket to /tmp/ directory in Lambda
+        s3.download_file(bucket_name, file_key, f'/tmp/{file_name}')
+
