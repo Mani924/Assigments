9b717cc4eec815eb47190095e78d011b8a6dc2ef
Mani924 <chamarthi509@gmail.com>
2023-06-05 18:30:31 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,33 +1,21 @@
 import boto3
-
-def lambda_handler(event, context):
-    s3 = boto3.client('s3')
-    bucket_name = 'your-bucket-name'
-    key_prefix = 'your-folder-prefix'  # Optional: If you want to copy files from a specific folder within the bucket
-    
-    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=key_prefix)
-    
-    for file in response['Contents']:
-        file_key = file['Key']
-        file_name = file_key.split('/')[-1]  # Extract the file name from the key
-        
-        # Copy file from S3 bucket to /tmp/ directory in Lambda
-        s3.download_file(bucket_name, file_key, f'/tmp/{file_name}')
-
-import boto3
 import os
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
     bucket_name = 'your-bucket-name'
-    key_prefix = 'your-folder-prefix'  # Optional: If you want to copy files from a specific folder within the bucket
+    folder_prefix = 'your-folder-prefix/'  # Include the trailing slash (/) to specify a folder
+    destination_directory = '/tmp/downloaded_files/'  # Specify the desired destination directory
     
-    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=key_prefix)
+    if not os.path.exists(destination_directory):
+        os.makedirs(destination_directory)
+    
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_prefix)
     
     for file in response['Contents']:
         file_key = file['Key']
-        file_name = file_key.split('/')[-1]  # Extract the file name from the key
+        file_name = os.path.basename(file_key)  # Extract the file name from the key
         
-        # Download file from S3 bucket to /tmp/filename in Lambda
-        with open('/tmp/' + file_name, 'wb') as f:
-            s3.download_fileobj(bucket_name, file_key, f)
+        # Download file from S3 folder to destination directory in Lambda
+        destination_path = os.path.join(destination_directory, file_name)
+        s3.download_file(bucket_name, file_key, destination_path)
