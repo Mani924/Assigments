98f7dc0d3154643c46f21903406936a8e74782b2
Mani924 <chamarthi509@gmail.com>
2023-08-02 01:06:32 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -3,4 +3,67 @@ if u_i.isalpha():
     print "input contans alphabates only"
 else:
     print "input not contains all alphabates "
-
+test_is_exempt {
+  # construct some input
+  input := {
+    "review": {
+      "object": {
+        "spec": {
+          "containers": [
+            {
+              "name": "test-container",
+              "image": "test-image",
+              "securityContext": {
+                "runAsUser": 150,
+                "runAsGroup": 150,
+                "supplementalGroups": [150],
+                "fsGroup": 150
+              }
+            }
+          ]
+        }
+      }
+    },
+    "parameters": {
+      "runAsUser": {
+        "rule": "MustRunAs",
+        "ranges": [
+          {
+            "min": 100,
+            "max": 200
+          }
+        ]
+      },
+      "runAsGroup": {
+        "rule": "MustRunAs",
+        "ranges": [
+          {
+            "min": 100,
+            "max": 200
+          }
+        ]
+      },
+      "supplementalGroups": {
+        "rule": "MustRunAs",
+        "ranges": [
+          {
+            "min": 100,
+            "max": 200
+          }
+        ]
+      },
+      "fsGroup": {
+        "rule": "MustRunAs",
+        "ranges": [
+          {
+            "min": 100,
+            "max": 200
+          }
+        ]
+      }
+    }
+  }
+  
+  # Assume container to be exempt
+  not is_exempt(input.review.object.spec.containers[0])
+}
